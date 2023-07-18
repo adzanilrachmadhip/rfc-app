ee6c513ac870db0f71a58ef40878771c6fe436c2
vagrant <vagrant@homestead>
2023-07-18 04:58:39 +0000

    first commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,28 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar" >
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link text-success" href="/">
+      <i class="bi bi-grid text-success"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link text-success" href="/table">
+      <i class="bi bi-layout-text-window-reverse text-success"></i>
+      <span>Table</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link text-success" href="index.html">
+      <i class="bi bi-bar-chart text-success"></i>
+      <span>Charts</span>
+    </a>
+  </li>
+
+</ul>
+
+</aside><!-- End Sidebar-->
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard', [
+        "title" => "Dashboard",
+        "username" => "HWD",
+        "roles" => "Admin",
+        "image" => "pakdekan.png",
+        "nama_lengkap" => "Dr. Helmy Widyantara"
+    ]);
+});
+
+Route::get('/table', function(){
+    return view('table',[
+        "title" => "Tabel",
+        "username" => "HWD",
+        "roles" => "Admin",
+        "image" => "pakdekan.png",
+        "nama_lengkap" => "Dr. Helmy Widyantara"
+    ]);
+});
